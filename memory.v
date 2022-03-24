0d1e099b57903868166f6bbdc2e61831a6a6bcde
sgreener27 <71909810+sgreener27@users.noreply.github.com>
2022-03-24 16:42:30 -0500

    updated signal names

--- memory.v
+++ memory.v
@@ -2,11 +2,12 @@ module memory(PC, flush, RdRq, ALURes, PCOut, DataOut, ALUOut);
 
     input [?:0]PC;
     input flush;
-    input [31:0]RdRq;
+    input [31:0]WriteDataIn;
     input [31:0]ALURes;
+    input WriteEn;
 
     output [?:0]PCOut;
-    output [31:0]DataOut;
+    output [31:0]ReadDataOut;
     output [31:0]ALUOut;
   
 endmodule
