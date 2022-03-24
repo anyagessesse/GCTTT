5128a3234dd9d1673254c2a7777c14556ed80539
sgreener27 <71909810+sgreener27@users.noreply.github.com>
2022-03-24 14:36:24 -0500

    Update memory.v

--- memory.v
+++ memory.v
@@ -1,1 +1,12 @@
+module memory(PC, flush, RdRq, ALURes, PCOut, DataOut, ALUOut);
 
+    input [?:0]PC;
+    input flush;
+    input [31:0]RdRq;
+    input [31:0]ALURes;
+
+    output [?:0]PCOut;
+    output [31:0]DataOut;
+    output [31:0]ALUOut;
+  
+endmodule
