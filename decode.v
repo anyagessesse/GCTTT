d2565abdbf8bfeb8dfe9a57ce224c77ef70c368a
sgreener27 <sgreener@wisc.edu>
2022-03-29 14:05:49 -0500

    updated decode (not done) and register file

--- decode.v
+++ decode.v
@@ -3,26 +3,45 @@ module decode(PC, PCPlus2, inst, PCIn, RqRd, Rs, JumpOrBranchHigh, RqRdOrImm, Rs
       input [?:0]PC, PCPlus2; //pc size?
       input [11:0]inst;  //[15:3] of whole instruction
 
-	output [?:0]PCOut; //pc size?
+      output [?:0]PCOut; //pc size?
       output [31:0]RqRd, Rs;
 
-	    // control signals
+      // control signals
       output JumpOrBranchHigh;
       output RqRdOrImm;
       output RsOrImm;
       output ALUCtrl;
+
+      //internal signals
+      wire halt, RqOrRq, ReqWrite;
+      wire [2:0]rea1_reg, read2_reg;
   
+
+      //assign internal signals
+      assign halt = (inst[15:12] == 4'b0001);
+      assign RdOrRq = inst[14];  //1 = Rd, 0 = Rq   **switch opcode for shift and branch?
+      assign writeReg = inst[15];  //1 = write to register, 0 = doesn't write to register
+
+      //pc select, keep same PC if halt is high
+      assign PCOut = halt ? PC : PCPlus2;
   
-      //seperate instructions
-  
-      //choose rq or rd
-  
-      //read/write registers
+      // choose Rd or Rq;
+      assign read1_reg = RdOrRq ? inst[11:9] : inst[5:3];
+      assign read2_reg = inst[8:6];
+
+      //read or write registers
+      registers(read1_reg, read2_reg, write_reg, WriteReg, read1_data, read2_data);
+
+      
+      
+      
   
       //control unit
       control(inst[15:12], JumpOrBranchHigh, RqRdOrImm, RsOrImm, ALUCtrl);
   
-      //pc select
+      //pc select, keep same PC if halt is high
+      assign PCOut = halt ? PC : PCPlus2;
+
   
 endmodule 
   
--- rf.v
+++ rf.v
@@ -1,15 +1,47 @@
-module registers(read1_reg, read2_reg, write_reg, WriteReg, read1_data, read2_data);
+module registers(read1_reg, read2_reg, write_reg, write_data, write_en, read1_data, read2_data);
   
   input [2:0]read1_reg, read2_reg, write_reg;
-  input WriteReg;
+  input [31:0]write_data;
+  input write_en;
   
   output [31:0]read1_data, read2_data;
   
  
-  //internal registers
+  // registers
+  reg [31:0] reg0, reg1, reg2, reg3, reg4, reg5, reg6, special;
+
+  // read data
+  assign read1_data = read1_reg = 3'b000 ? reg0 :
+		      read1_reg = 3'b001 ? reg1 :
+		      read1_reg = 3'b010 ? reg2 :
+                      read1_reg = 3'b011 ? reg3 :
+                      read1_reg = 3'b100 ? reg4 :
+                      read1_reg = 3'b101 ? reg5 :
+                      read1_reg = 3'b110 ? reg6 : special;
+
+  assign read2_data = read2_reg = 3'b000 ? reg0 :
+		      read2_reg = 3'b001 ? reg1 :
+		      read2_reg = 3'b010 ? reg2 :
+                      read2_reg = 3'b011 ? reg3 :
+                      read2_reg = 3'b100 ? reg4 :
+                      read2_reg = 3'b101 ? reg5 :
+                      read2_reg = 3'b110 ? reg6 : special;
   
-  //read results
-  
-  //write
-  
+
+  //write data
+  always @(posedge clk) begin   
+	if (write_en) begin
+        	case (write_reg) begin
+			3'b000: reg0 <= write_data;
+			3'b001: reg1 <= write_data;
+			3'b010: reg2 <= write_data;
+			3'b011: reg3 <= write_data;
+			3'b100: reg4 <= write_data;
+			3'b101: reg5 <= write_data;
+			3'b110: reg6 <= write_data;
+			default: special <= write_data;
+		endcase
+	end
+  end 
+
 endmodule 
