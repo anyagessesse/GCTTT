d09533dae5e895a724fb49b0d09ef9decbc87200
sgreener27 <sgreener@wisc.edu>
2022-03-29 14:39:14 -0500

    updated decode

--- decode.v
+++ decode.v
@@ -1,10 +1,11 @@
-module decode(PC, PCPlus2, inst, PCIn, RqRd, Rs, JumpOrBranchHigh, RqRdOrImm, RsOrImm, ALUCtrl);
+module decode(PC, PCPlus2, inst, PCOut, RdRq, Rs, write_en, JumpOrBranchHigh, RqRdOrImm, RsOrImm, ALUCtrl);
 
       input [?:0]PC, PCPlus2; //pc size?
       input [11:0]inst;  //[15:3] of whole instruction
 
       output [?:0]PCOut; //pc size?
-      output [31:0]RqRd, Rs;
+      output [2:0]RdRq, Rs;  //go to register file
+      output write_en;  //pass through stages for write back
 
       // control signals
       output JumpOrBranchHigh;
@@ -12,35 +13,18 @@ module decode(PC, PCPlus2, inst, PCIn, RqRd, Rs, JumpOrBranchHigh, RqRdOrImm, Rs
       output RsOrImm;
       output ALUCtrl;
 
-      //internal signals
-      wire halt, RqOrRq, ReqWrite;
-      wire [2:0]rea1_reg, read2_reg;
-  
-
-      //assign internal signals
-      assign halt = (inst[15:12] == 4'b0001);
-      assign RdOrRq = inst[14];  //1 = Rd, 0 = Rq   **switch opcode for shift and branch?
-      assign writeReg = inst[15];  //1 = write to register, 0 = doesn't write to register
-
-      //pc select, keep same PC if halt is high
-      assign PCOut = halt ? PC : PCPlus2;
-  
-      // choose Rd or Rq;
-      assign read1_reg = RdOrRq ? inst[11:9] : inst[5:3];
-      assign read2_reg = inst[8:6];
-
-      //read or write registers
-      registers(read1_reg, read2_reg, write_reg, WriteReg, read1_data, read2_data);
-
-      
-      
-      
-  
       //control unit
       control(inst[15:12], JumpOrBranchHigh, RqRdOrImm, RsOrImm, ALUCtrl);
+
+      // move to control file
+      assign JumpOrBranchHigh = 
   
       //pc select, keep same PC if halt is high
-      assign PCOut = halt ? PC : PCPlus2;
+      assign PCOut = (inst[15:12] == 3'b0001) ? PC : PCPlus2;
+
+      // select rd or rq register, pass register numbers to rf
+      assign RdRq = inst[14] ? inst[11:9] : inst[5:3];
+      assign Rs = inst[8:6];
 
   
 endmodule 
