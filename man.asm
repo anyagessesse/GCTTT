355b1b085fafbe40edf8576c1cce03b5c966033f
klassiterUW <60987358+klassiterUW@users.noreply.github.com>
2022-04-07 18:42:58 -0500

    Shift with better values

--- man.asm
+++ man.asm
@@ -1,4 +1,28 @@
-MV0 R1, 1
+################################################################################
+# initialize registers
+################################################################################
+MV0 R0, 0	# R0 <- 0
+NOP
+NOP
+NOP
+NOP
+MV1 R0, 0
+NOP
+NOP
+NOP
+NOP
+MV2 R0, 0
+NOP
+NOP
+NOP
+NOP
+MV3 R0, 0
+NOP
+NOP
+NOP
+NOP
+
+MV0 R1, 1	# R1 <- 1
 NOP
 NOP
 NOP
@@ -18,7 +42,29 @@ NOP
 NOP
 NOP
 NOP
-MV0 R3, 0
+
+MV0 R2, 2	# R2 <- 2
+NOP
+NOP
+NOP
+NOP
+MV1 R2, 0
+NOP
+NOP
+NOP
+NOP
+MV2 R2, 0
+NOP
+NOP
+NOP
+NOP
+MV3 R2, 0
+NOP
+NOP
+NOP
+NOP
+
+MV0 R3, 3	# R3 <- 3
 NOP
 NOP
 NOP
@@ -38,66 +84,88 @@ NOP
 NOP
 NOP
 NOP
-ADD R2, R3, R1
+
+MV0 R4, 4	# R4 <- 4
 NOP
 NOP
 NOP
 NOP
-SUB R2, R3, R1
+MV1 R4, 0
 NOP
 NOP
 NOP
 NOP
-AND R2, R3, R1
+MV2 R4, 0
 NOP
 NOP
 NOP
 NOP
-OR R2, R3, R1
+MV3 R4, 0
 NOP
 NOP
 NOP
 NOP
-NOT R2, R3
+
+MV0 R5, 3	# R5 <- 25
 NOP
 NOP
 NOP
 NOP
-XOR R2, R3, R1
+MV1 R5, 0
 NOP
 NOP
 NOP
 NOP
-SU R2, R3
+MV2 R5, 0
 NOP
 NOP
 NOP
 NOP
-SL R2, R3
+MV3 R5, 0
 NOP
 NOP
 NOP
 NOP
-SHRA R2, R3, R1
+
+################################################################################
+# operations
+################################################################################
+SU R0, R5
 NOP
 NOP
 NOP
 NOP
-SHRL R2, R3, R1
+
+SL R0, R5
 NOP
 NOP
 NOP
 NOP
-ROR R2, R3, R1
+
+SHRA R0, R1, R2
 NOP
 NOP
 NOP
 NOP
-SHL R2, R3, R1
+
+SHRL R0, R2, R3
 NOP
 NOP
 NOP
 NOP
-ROL R2, R3, R1
+
+ROR R0, R3, R4
+NOP
+NOP
+NOP
+NOP
+
+SHL R0, R4, R5
+NOP
+NOP
+NOP
+NOP
+
+ROL R0, R5, R4
 HALT
 
