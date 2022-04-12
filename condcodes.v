64606de2786a9ab503a6ced57e8febf67e117c41
Anya Gessesse <55361826+anyagessesse@users.noreply.github.com>
2022-04-12 16:13:30 -0500

    Update condcodes.v

--- condcodes.v
+++ condcodes.v
@@ -1,8 +1,5 @@
-module condcodes(ALUCtrl,ALUOut,A,B,EQ,LT,GT,LE,GE,NE);
+module condcodes(B,EQ,LT,GT,LE,GE,NE);
 
-	input [3:0]ALUCtrl;
-	input [31:0]ALUOut;
-	input [31:0]A;
 	input [31:0]B;
 	
 	output EQ;
@@ -14,31 +11,21 @@ module condcodes(ALUCtrl,ALUOut,A,B,EQ,LT,GT,LE,GE,NE);
 	
 	wire neg;
 	wire zero;
-	wire overflow;
-	wire adding;
-	wire subtracting;
-	wire overflow1,overflow2;
+	
+	// comparing B to zero
 	
 	// determine neg and zero flags
-	assign neg = ALUOut[31];
-	assign zero = ~|ALUOut;
+	assign neg = B[31];
+	assign zero = ~|B;
 	
-	// determine overflow
-	assign adding = (ALUCtrl == 4'b1110) ? 1'b1 : 1'b0;
-	assign subtracting = (ALUCtrl == 4'b1101) ? 1'b1 : 1'b0;
-	
-	assign overflow1 = (ALUOut[31] & ~A[31] & ~B[31]) | (~ALUOut[31] & A[31] & B[31]);
-	assign overflow2 = (ALUOut[31] & A[31] & ~B[31]) | (~ALUOut[31] & ~A[31] & B[31]);
-	
-	assign overflow = (adding & overflow1) | (subtracting & overflow2);
 	
 	// set all condition codes
-	assign EQ = ~overflow & zero;
-	assign LT = ~overflow & ~zero & neg;
-	assign GT = ~overflow & ~zero & ~neg;
-	assign LE = ~overflow & (zero | neg);
-	assign GE = ~overflow & (zero | ~neg);
-	assign NE = ~overflow & ~zero;
+	assign EQ = zero;
+	assign LT = ~zero & neg;
+	assign GT = ~zero & ~neg;
+	assign LE = zero | neg;
+	assign GE = zero | ~neg;
+	assign NE = ~zero;
 
 
 endmodule
