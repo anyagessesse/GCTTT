a4ae41428c360ac3028f18b953df2efda7db146a
sgreener27 <sgreener@wisc.edu>
2022-04-19 18:39:03 -0500

    fixed memory

--- memory.v
+++ memory.v
@@ -21,54 +21,6 @@ module memory(flush, RdRqIn, ALURes, Mem_Write, MemRead,
 
     assign WriteRegDataOut = MemRead ? MemDataOut : ALURes;
 
-
-    RAM0 Sdram_Control (
-		//	HOST Side
-    	RESET_N,
-	CLK,
-		//	FIFO Write Side 1
-    	WR1_DATA,
-		WR1,
-		WR1_ADDR,
-		WR1_MAX_ADDR,
-		WR1_LENGTH,
-		WR1_LOAD,
-		WR1_CLK,
-		//	FIFO Write Side 2
-    	WR2_DATA,
-		WR2,
-		WR2_ADDR,
-		WR2_MAX_ADDR,
-		WR2_LENGTH,
-		WR2_LOAD,
-		WR2_CLK,
-		//	FIFO Read Side 1
-    	RD1_DATA,
-		RD1,
-		RD1_ADDR,
-		RD1_MAX_ADDR,
-		RD1_LENGTH,
-		RD1_LOAD,	
-		RD1_CLK,
-		//	FIFO Read Side 2
-    	RD2_DATA,
-		RD2,
-		RD2_ADDR,
-		RD2_MAX_ADDR,
-		RD2_LENGTH,
-		RD2_LOAD,
-		RD2_CLK,
-		//	SDRAM Side
-    	SA,
-    	BA,
-    	CS_N,
-   	CKE,
-    	RAS_N,
-    	CAS_N,
-    	WE_N,
-    	DQ,
-    	DQM
-	);
     //memory stuff here :) use WriteDataIn, ALURes, Mem_Write
     //memory2c MEM0(.data_out(data_imm), .data_in(RdRqIn[15:0]), .addr(ALURes[15:0]), .enable(1'b1), .wr(Mem_Write), .createdump(1'b0), .clk(clk), .rst(rst));
 
