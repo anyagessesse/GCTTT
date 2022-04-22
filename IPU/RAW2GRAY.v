345da90ee41413e143a1364132f49f34f338a73a
nasusilo <71673032+nasusilo@users.noreply.github.com>
2022-04-22 01:10:43 -0500

    Add files via upload

--- IPU/RAW2GRAY.v
+++ IPU/RAW2GRAY.v
@@ -48,7 +48,9 @@ module RAW2GRAY(oGrey,
 				iDVAL,
 				iCLK,
 				iRST,
-				oDetect
+				oDetect,
+				loc,
+				iFrame,
 				);
 
 input	[10:0]	iX_Cont;
@@ -57,22 +59,116 @@ input	[11:0]	iDATA;
 input			iDVAL;
 input			iCLK;
 input			iRST;
+input [1:0]iFrame;
 output	[11:0]	oGrey;
 output			oDVAL;
 output reg oDetect;
+output reg [3:0] loc;
 wire	[11:0]	mDATA_0;
 wire	[11:0]	mDATA_1;
 reg		[11:0]	mDATAd_0;
 reg		[11:0]	mDATAd_1;
 reg		[13:0]	mCCD_G;
 reg				mDVAL;
+reg [3:0] curr_loc;
+reg [3:0] prev_loc, prev_2loc;
+wire interrupt;
+reg internal_int;
+
+localparam [10:0] edge_left = 11'd220;
+localparam [10:0] edge_right = 11'd1025;
+localparam [10:0] edge_top = 11'd70;
+localparam [10:0] edge_bottom = 11'd860;
+localparam [10:0] left = 11'd482;
+localparam [10:0] right = 11'd760;
+localparam [10:0] top = 11'd328;
+localparam [10:0] bottom = 11'd604;
+ 
+
+always @(posedge iCLK or negedge iRST) begin
+	if (!iRST) begin
+		prev_loc <= 4'hF;
+		prev_2loc <= 4'hF;
+	end
+	else begin
+		prev_loc <= curr_loc;
+		prev_2loc <= prev_loc;
+	end
+end
+
+assign interrupt = (prev_loc != curr_loc) & (~&curr_loc);
+
+always @(posedge iCLK or negedge iRST) begin
+	if(!iRST) 
+		internal_int <= 1'b0;
+	else if (&iFrame) 
+		internal_int <= 1'b0;
+	else if (interrupt)
+		internal_int <= 1'b1;
+end
+		
+always @(posedge iCLK or negedge iRST) begin
+	if (!iRST) begin
+		loc <= 4'hF;
+	end
+	else if (internal_int & (&iFrame)) begin
+		loc <= curr_loc;
+	end
+end
 
 
-//assign oGrey = (iX_Cont >= 11'd500 & iX_Cont <= 11'd780 & iY_Cont >= 11'd80 & iY_Cont <= 11'd880) ?  mCCD_G[13:2] : 12'h000;
-//assign oGrey = (iX_Cont >= 11'd220 & iX_Cont <= 11'd1025 & iY_Cont >= 11'd70 & iY_Cont <= 11'd860) ? mCCD_G[13:2] : 12'h000;
-assign oGrey = (mCCD_G[13:2] < 1000 & iX_Cont >= 11'd220 & iX_Cont <= 11'd1025 & iY_Cont >= 11'd70 & iY_Cont <= 11'd860) ? 12'hFFF : 12'h000;
+//assign oGrey = (iX_Cont >= 11'd220 & iX_Cont <= 11'd1025 & iY_Cont >= 11'd70 & iY_Cont <= 11'd860) ?  mCCD_G[13:2] : 12'h000;
+assign oGrey = (iX_Cont >= edge_left & iX_Cont <= edge_right & iY_Cont >= edge_top & iY_Cont <= edge_bottom) ? mCCD_G[13:2] : 12'h000;
+//assign oGrey = (mCCD_G[13:2] < 1000 & iX_Cont >= 11'd220 & iX_Cont <= 11'd1025 & iY_Cont >= 11'd70 & iY_Cont <= 11'd860) ? 12'hFFF : 12'h000;
 //assign oGrey = mCCD_G[13:2];
 //assign	oGrey	=	(mCCD_G[13:2] < 1000 & iX_Cont >= 11'd500 & iX_Cont <= 11'd780 & iY_Cont >= 11'd80 & iY_Cont <= 11'd880) ? 12'hFFF : 12'h000;
+
+//assign loc = curr_loc;
+
+always @(posedge iCLK or negedge iRST) begin
+	if (!iRST) begin
+		curr_loc <= 4'hF;
+	end
+	else if (&iFrame) 
+		curr_loc <= 4'hF;
+	else if (mCCD_G[13:2] < 1000 & iX_Cont >= edge_left & iX_Cont <= left & iY_Cont >= edge_top & iY_Cont <= top) begin
+		curr_loc <= 4'b0000;
+	end
+	else if (mCCD_G[13:2] < 1000 & iX_Cont >= edge_left & iX_Cont <= left & iY_Cont >= top & iY_Cont <= bottom) begin
+		if (curr_loc[1:0] > 2'h0)
+			curr_loc <= 4'b0100;
+	end
+	else if (mCCD_G[13:2] < 1000 & iX_Cont >= edge_left & iX_Cont <= left & iY_Cont >= bottom & iY_Cont <= edge_bottom) begin
+		if (curr_loc[1:0] > 2'h0) 
+			curr_loc <= 4'b1000;
+	end
+	else if (mCCD_G[13:2] < 1000 & iX_Cont >= left & iX_Cont <= right & iY_Cont >= edge_top & iY_Cont <= top) begin
+		if (curr_loc[1:0] > 2'h1) 
+			curr_loc <= 4'b0001;
+	end
+	else if (mCCD_G[13:2] < 1000 & iX_Cont >= left & iX_Cont <= right & iY_Cont >= top & iY_Cont <= bottom) begin
+		if (curr_loc[1:0] > 2'h1)
+			curr_loc <= 4'b0101;
+	end
+	else if (mCCD_G[13:2] < 1000 & iX_Cont >= left & iX_Cont <= right & iY_Cont >= bottom & iY_Cont <= edge_bottom) begin
+		if (curr_loc[1:0] > 2'h1)
+			curr_loc <= 4'b1001;
+	end
+	else if (mCCD_G[13:2] < 1000 & iX_Cont >= right & iX_Cont <= edge_right & iY_Cont >= edge_top & iY_Cont <= top) begin
+		if (curr_loc[1:0] > 2'h2)
+			curr_loc <= 4'b0010;
+	end
+	else if (mCCD_G[13:2] < 1000 & iX_Cont >= right & iX_Cont <= edge_right & iY_Cont >= top & iY_Cont <= bottom) begin
+		if (curr_loc[1:0] > 2'h2)
+			curr_loc <= 4'b0110;
+	end
+	else if (mCCD_G[13:2] < 1000 & iX_Cont >= right & iX_Cont <= edge_right & iY_Cont >= bottom & iY_Cont <= edge_bottom) begin
+		if (curr_loc[1:0] > 2'h2)
+			curr_loc <= 4'b1010;
+	end
+	else
+		curr_loc <= curr_loc;
+end
 
 always @(posedge iCLK or negedge iRST) begin
 	if(!iRST)
