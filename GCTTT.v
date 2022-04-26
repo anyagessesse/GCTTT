53420cce3cb598916d08c37f00cd55db9ff50ad5
Anya Gessesse <55361826+anyagessesse@users.noreply.github.com>
2022-04-26 16:06:13 -0500

    Add files via upload

--- GCTTT.v
+++ GCTTT.v
@@ -3,7 +3,29 @@ module GCTTT(
 	input [3:0]KEY,
 	output [9:0]LEDR);
 
-proc p0(.clk(CLOCK_50), .rst(KEY[0]), .leds(LEDR));
+
+
+//assign LEDR = 10'b1111111111;
+//reg [27:0]count;
+
+assign LEDR = CLOCK_50 ? 10'b1111111111: 10'b0000000000;
+
+
+//always @(posedge CLOCK_50)begin
+//	count = count + 1;
+
+
+//	if(KEY[0])
+//		LEDR = 10'b0000000000;
+
+//	else if(count == 50000000)
+//		LEDR = LEDR + 1;
+
+//end
+
+
+
+//proc p0(.clk(CLOCK_50), .rst(KEY[0]), .leds(LEDR));
 
 endmodule
 
