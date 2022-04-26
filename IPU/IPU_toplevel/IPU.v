540a9428a6205a0ca3eac1aafd5889e6dfa9c565
nasusilo <71673032+nasusilo@users.noreply.github.com>
2022-04-26 18:40:26 -0500

    Add files via upload

--- IPU/IPU_toplevel/IPU.v
+++ IPU/IPU_toplevel/IPU.v
@@ -1,1 +1,112 @@
+module IPU(
 
+      ///////// CLOCK2 /////////
+      input              CLOCK2_50,
+
+      ///////// SW /////////
+      input       [9:0]  SW,
+
+		
+		//////////// GPIO1, GPIO1 connect to D5M - 5M Pixel Camera //////////
+	   input		   [11:0] D5M_D,
+      input		          D5M_FVAL,
+      input		          D5M_LVAL,
+      input		          D5M_PIXLCLK,
+      output		       D5M_SCLK, // I2C output1
+      inout		          D5M_SDATA, // I2C output2
+      input		          D5M_STROBE,
+	  output			[3:0] loc, // grid number --- key output
+	  output 			oInterrupt // finger detect interrupt --- key output
+	  
+);
+
+
+//=======================================================
+//  REG/WIRE declarations
+//=======================================================
+wire			 [11:0]			mCCD_DATA;
+wire								mCCD_DVAL;
+wire	       [15:0]			X_Cont;
+wire	       [15:0]			Y_Cont;
+wire	       [31:0]			Frame_Cont;
+wire								DLY_RST_0;
+wire								DLY_RST_1;
+wire								DLY_RST_2;
+wire								DLY_RST_3;
+wire								DLY_RST_4;
+
+reg		    [11:0]			rCCD_DATA;
+reg								rCCD_LVAL;
+reg								rCCD_FVAL;
+
+
+
+//power on start
+wire             				auto_start;
+//=======================================================
+//  Structural coding
+//=======================================================
+
+//D5M read 
+always@(posedge D5M_PIXLCLK)
+begin
+	rCCD_DATA	<=	D5M_D;
+	rCCD_LVAL	<=	D5M_LVAL;
+	rCCD_FVAL	<=	D5M_FVAL;
+end
+
+
+//auto start when power on
+assign auto_start = ((KEY[0])&&(DLY_RST_3)&&(!DLY_RST_4))? 1'b1:1'b0;
+//Reset module
+
+Reset_Delay			ipu1	(	
+							.iCLK(CLOCK_50),
+							.iRST(KEY[0]),
+							.oRST_0(DLY_RST_0),
+							.oRST_1(DLY_RST_1),
+							.oRST_2(DLY_RST_2),
+							.oRST_3(DLY_RST_3),
+							.oRST_4(DLY_RST_4)
+						   );
+//D5M image capture
+CCD_Capture			ipu2	(	
+							.oDATA(mCCD_DATA),
+							.oDVAL(mCCD_DVAL),
+							.oX_Cont(X_Cont),
+							.oY_Cont(Y_Cont),
+							.oFrame_Cont(Frame_Cont),
+							.iDATA(rCCD_DATA),
+							.iFVAL(rCCD_FVAL),
+							.iLVAL(rCCD_LVAL),
+							.iSTART(!KEY[3]|auto_start),
+							.iEND(!KEY[2]),
+							.iCLK(~D5M_PIXLCLK),
+							.iRST(DLY_RST_2)
+						   );
+//D5M raw date convert to RGB data
+						   
+imageProc			ipu3	(	
+							.iCLK(D5M_PIXLCLK),
+							.iRST(DLY_RST_1),
+							.iDATA(mCCD_DATA),
+							.iDVAL(mCCD_DVAL),
+							.iX_Cont(X_Cont),
+							.iY_Cont(Y_Cont),
+							.oInterrupt(oInterrupt),
+							.loc(loc),
+							.iFrame(Frame_Cont),
+						   );
+						   
+			
+I2C_CCD_Config 	ipu4	(	//	Host Side
+							.iCLK(CLOCK2_50),
+							.iRST_N(DLY_RST_2),
+							.iEXPOSURE_ADJ(KEY[1]),
+							.iEXPOSURE_DEC_p(SW[0]),
+							.iZOOM_MODE_SW(SW[9]),
+							//	I2C Side
+							.I2C_SCLK(D5M_SCLK),
+							.I2C_SDAT(D5M_SDATA)
+						   );
+endmodule						   
---
+++ IPU/IPU_toplevel/imageProc.v
@@ -0,0 +1,213 @@
+// --------------------------------------------------------------------
+// Copyright (c) 20057 by Terasic Technologies Inc. 
+// --------------------------------------------------------------------
+//
+// Permission:
+//
+//   Terasic grants permission to use and modify this code for use
+//   in synthesis for all Terasic Development Boards and Altera Development 
+//   Kits made by Terasic.  Other use of this code, including the selling 
+//   ,duplication, or modification of any portion is strictly prohibited.
+//
+// Disclaimer:
+//
+//   This VHDL/Verilog or C/C++ source code is intended as a design reference
+//   which illustrates how these types of functions can be implemented.
+//   It is the user's responsibility to verify their design for
+//   consistency and functionality through the use of formal
+//   verification methods.  Terasic provides no warranty regarding the use 
+//   or functionality of this code.
+//
+// --------------------------------------------------------------------
+//           
+//                     Terasic Technologies Inc
+//                     356 Fu-Shin E. Rd Sec. 1. JhuBei City,
+//                     HsinChu County, Taiwan
+//                     302
+//
+//                     web: http://www.terasic.com/
+//                     email: support@terasic.com
+//
+// --------------------------------------------------------------------
+//
+// Major Functions:	RAW2RGB
+//
+// --------------------------------------------------------------------
+//
+// Revision History :
+// --------------------------------------------------------------------
+//   Ver  :| Author            :| Mod. Date :| 		Changes Made:
+//   V1.0 :| Johnny Fan        :| 07/08/01  :|      Initial Revision
+// --------------------------------------------------------------------
+
+module imageProc(
+				iX_Cont,
+				iY_Cont,
+				iDATA,
+				iDVAL,
+				iCLK,
+				iRST,
+				oInterrupt,
+				loc,
+				iFrame,
+				);
+
+input	[10:0]	iX_Cont;
+input	[10:0]	iY_Cont;
+input	[11:0]	iDATA;
+input			iDVAL;
+input			iCLK;
+input			iRST;
+input iFrame;
+output interrupt;
+output reg [3:0] loc;
+
+
+reg oDetect;
+
+wire	[11:0]	mDATA_0;
+wire	[11:0]	mDATA_1;
+reg		[11:0]	mDATAd_0;
+reg		[11:0]	mDATAd_1;
+reg		[13:0]	mCCD_G;
+reg				mDVAL;
+reg [3:0] curr_loc;
+reg [3:0] prev_loc, prev_2loc;
+
+reg internal_int;
+
+localparam [10:0] edge_left = 11'd220;
+localparam [10:0] edge_right = 11'd1025;
+localparam [10:0] edge_top = 11'd70;
+localparam [10:0] edge_bottom = 11'd860;
+localparam [10:0] left = 11'd482;
+localparam [10:0] right = 11'd760;
+localparam [10:0] top = 11'd328;
+localparam [10:0] bottom = 11'd604;
+ 
+
+always @(posedge iCLK or negedge iRST) begin
+	if (!iRST) begin
+		prev_loc <= 4'hF;
+		prev_2loc <= 4'hF;
+	end
+	else begin
+		prev_loc <= curr_loc;
+		prev_2loc <= prev_loc;
+	end
+end
+
+assign interrupt = (prev_loc != curr_loc) & (~&curr_loc);
+
+always @(posedge iCLK or negedge iRST) begin
+	if(!iRST) 
+		internal_int <= 1'b0;
+	else if (&iFrame) 
+		internal_int <= 1'b0;
+	else if (interrupt)
+		internal_int <= 1'b1;
+end
+		
+always @(posedge iCLK or negedge iRST) begin
+	if (!iRST) begin
+		loc <= 4'hF;
+	end
+	else if (internal_int & (&iFrame)) begin
+		loc <= {curr_loc[1:0],curr_loc[3:2]};
+	end
+end
+
+
+//assign oGrey = (iX_Cont >= 11'd220 & iX_Cont <= 11'd1025 & iY_Cont >= 11'd70 & iY_Cont <= 11'd860) ?  mCCD_G[13:2] : 12'h000;
+//assign oGrey = (mCCD_G[13:2] < 1000 & iX_Cont >= edge_left & iX_Cont <= edge_right & iY_Cont >= edge_top & iY_Cont <= edge_bottom) ? mCCD_G[13:2] : 12'h000;
+
+//assign oGrey = (iX_Cont >= edge_left & iX_Cont <= edge_right & iY_Cont >= edge_top& iY_Cont <= edge_bottom) ? mCCD_G[13:2] : 12'h000;
+
+//assign oGrey = mCCD_G[13:2];
+//assign oGrey = (mCCD_G[13:2] < 1000 & iX_Cont >= 11'd220 & iX_Cont <= 11'd1025 & iY_Cont >= 11'd70 & iY_Cont <= 11'd860) ? 12'hFFF : 12'h000;
+//assign oGrey = mCCD_G[13:2];
+//assign	oGrey	=	(mCCD_G[13:2] < 1000 & iX_Cont >= 11'd500 & iX_Cont <= 11'd780 & iY_Cont >= 11'd80 & iY_Cont <= 11'd880) ? 12'hFFF : 12'h000;
+
+//assign loc = curr_loc;
+
+always @(posedge iCLK or negedge iRST) begin
+	if (!iRST) begin
+		curr_loc <= 4'hF;
+	end
+	else if (&iFrame) 
+		curr_loc <= 4'hF;
+	else if (mCCD_G[13:2] < 1000 & iX_Cont >= edge_left & iX_Cont <= left & iY_Cont >= edge_top & iY_Cont <= top) begin
+		curr_loc <= 4'b0000;
+	end
+	else if (mCCD_G[13:2] < 1000 & iX_Cont >= edge_left & iX_Cont <= left & iY_Cont >= top & iY_Cont <= bottom) begin
+		if (curr_loc[1:0] > 2'h0)
+			curr_loc <= 4'b0100;
+	end
+	else if (mCCD_G[13:2] < 1000 & iX_Cont >= edge_left & iX_Cont <= left & iY_Cont >= bottom & iY_Cont <= edge_bottom) begin
+		if (curr_loc[1:0] > 2'h0) 
+			curr_loc <= 4'b1000;
+	end
+	else if (mCCD_G[13:2] < 1000 & iX_Cont >= left & iX_Cont <= right & iY_Cont >= edge_top & iY_Cont <= top) begin
+		if (curr_loc[1:0] > 2'h1) 
+			curr_loc <= 4'b0001;
+	end
+	else if (mCCD_G[13:2] < 1000 & iX_Cont >= left & iX_Cont <= right & iY_Cont >= top & iY_Cont <= bottom) begin
+		if (curr_loc[1:0] > 2'h1)
+			curr_loc <= 4'b0101;
+	end
+	else if (mCCD_G[13:2] < 1000 & iX_Cont >= left & iX_Cont <= right & iY_Cont >= bottom & iY_Cont <= edge_bottom) begin
+		if (curr_loc[1:0] > 2'h1)
+			curr_loc <= 4'b1001;
+	end
+	else if (mCCD_G[13:2] < 1000 & iX_Cont >= right & iX_Cont <= edge_right & iY_Cont >= edge_top & iY_Cont <= top) begin
+		if (curr_loc[1:0] > 2'h2)
+			curr_loc <= 4'b0010;
+	end
+	else if (mCCD_G[13:2] < 1000 & iX_Cont >= right & iX_Cont <= edge_right & iY_Cont >= top & iY_Cont <= bottom) begin
+		if (curr_loc[1:0] > 2'h2)
+			curr_loc <= 4'b0110;
+	end
+	else if (mCCD_G[13:2] < 1000 & iX_Cont >= right & iX_Cont <= edge_right & iY_Cont >= bottom & iY_Cont <= edge_bottom) begin
+		if (curr_loc[1:0] > 2'h2)
+			curr_loc <= 4'b1010;
+	end
+	else
+		curr_loc <= curr_loc;
+end
+
+always @(posedge iCLK or negedge iRST) begin
+	if(!iRST)
+		oDetect <= 1'b0;
+	else if (mCCD_G[13:2] < 1000 & iX_Cont >= 11'd500 & iX_Cont <= 11'd780 & iY_Cont >= 11'd70 & iY_Cont <= 11'd875)
+		oDetect <= 1'b1;
+end
+//assign oDetect = mCCD_G[13:2] < 1000 & iX_Cont >= 11'd500 & iX_Cont <= 11'd780;
+//assign	oDVAL	=	mDVAL;
+
+Line_Buffer1 	u0	(	.clken(iDVAL),
+						.clock(iCLK),
+						.shiftin(iDATA),
+						.taps0x(mDATA_1),
+						.taps1x(mDATA_0)	);
+
+always@(posedge iCLK or negedge iRST)
+begin
+	if(!iRST)
+	begin
+		mCCD_G	<=	0;
+		mDATAd_0<=	0;
+		mDATAd_1<=	0;
+		mDVAL	<=	0;
+	end
+	else
+	begin
+		mDATAd_0	<=	mDATA_0;
+		mDATAd_1	<=	mDATA_1;
+		mDVAL		<=	{iY_Cont[0]|iX_Cont[0]}	?	1'b0	:	iDVAL;
+		mCCD_G <= mDATA_0 + mDATA_1 + mDATAd_0 + mDATAd_1;
+	end
+end
+
+endmodule
+
+
