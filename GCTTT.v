4f492dc723018bfad384383dc6e96786438a9152
sgreener27 <sgreener@wisc.edu>
2022-05-02 13:50:55 -0500

    updated main file

--- GCTTT.v
+++ GCTTT.v
@@ -1,31 +1,32 @@
 module GCTTT(
 	input CLOCK_50,
 	input [3:0]KEY,
-	output [9:0]LEDR);
+	output [9:0]LEDR
+      	input  [9:0]SW,
+	input  [11:0] D5M_D,
+      	input	D5M_FVAL,
+      	input	D5M_LVAL,
+      	input	D5M_PIXLCLK,
+      	output	D5M_SCLK, // I2C output1
+      	inout	D5M_SDATA, // I2C output2
+      	input	D5M_STROBE);
 
 
+wire [3:0]loc,grid_loc_out;
+wire oInt,int_ack;
+wire pixel_en;
+wire [31:0]pixel_addr;
+wire pixel_value;
 
-//assign LEDR = 10'b1111111111;
-//reg [27:0]count;
+//need to connect int_ack to IPU, pixel_addr, pixel_value, and pixel_en to buffer
 
-assign LEDR = CLOCK_50 ? 10'b1111111111: 10'b0000000000;
+proc	PROC0(.clk(CLOCK_50),.rst(KEY[0]),.leds(LEDR),.pixel_addr(pixel_addr),.pixel_value(pixel_value),.pixel_en(pixel_en),.ipu_int(oInt),.int_ack(int_act),.grid_coord(grid_loc_out));
 
+IPU	IPU0(.CLOCK2_50(CLOCK_50),.SW(SW),.D5M_D(D5M_D),.D5M_FVAL(D5M_FVAL),.D5M_LVAL(D5M_LVAL),
+     		.D5M_PIXLCLK(D5M_PIXLCLK),.D5M_SCLK(D5M_SCLK),.D5M_SDATA(D5M_SDATA), 
+      		.D5M_STROBE(D5M_STROBE),.loc(loc),.oInterrupt(oInt));
 
-//always @(posedge CLOCK_50)begin
-//	count = count + 1;
-
-
-//	if(KEY[0])
-//		LEDR = 10'b0000000000;
-
-//	else if(count == 50000000)
-//		LEDR = LEDR + 1;
-
-//end
-
-
-
-//proc p0(.clk(CLOCK_50), .rst(KEY[0]), .leds(LEDR));
+grid_coord	GRID0(.clk(CLOCK_50),.rst(KEY[0]),.write_en(oInt),.coord_in(loc),.coord_out(grid_loc_out));
 
 endmodule
 
